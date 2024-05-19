a74a8bef2666c2097224d3f76e3d7e4d9d112a5d
Ismat-Samadov <ismetsemedov@gmail.com>
2024-05-19 15:54:07 +0400

    some task solution from udemy

--- application_two/app_two/views.py
+++ application_two/app_two/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello Python")
+    return HttpResponse("<em>My Second App</em>")
--- application_two/application_two/urls.py
+++ application_two/application_two/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for ProTwo project.
+URL configuration for application_two project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from first_app import views
+from app_two import views
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('',views.index,name = 'index'),
     path('admin/', admin.site.urls),
 ]
