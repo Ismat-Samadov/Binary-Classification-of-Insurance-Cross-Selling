338804010067dc0d4acd85fdc51925de84f894d7
Ismat-Samadov <ismetsemedov@gmail.com>
2024-05-26 20:02:37 +0400

    template challenges done

--- temp_project/second_app/urls.py
+++ temp_project/second_app/urls.py
@@ -1,9 +1,8 @@
-from first_app import views
+from second_app import views
 from django.urls import path
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 
-urlpatterns = [path('', views.index, name='index'),
-               path('home', views.home, name='home'),
+urlpatterns = [path('', views.help, name='help'),
                ]
